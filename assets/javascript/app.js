6a05af97eefa644ce8f5a349efb386a058ee8d30
picasso <46543347+djreichel@users.noreply.github.com>
2019-03-09 09:41:24 -0500

    another update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,9 +3,9 @@
 var topics = ["dogs", "cats", "fish", "birds", "squirrels", "rabbits", "ferrets", "chipmunks", "hamsters", "gerbils", "spiders", "monkees", "elephants", "rhinos"];
 
 // Grabbing and storing the data-animal property value from the button
-var animal = $(this).attr("animal-buttons");
+//var animal = $(this).attr("#animal-buttons");
 // Constructing a queryURL using the animal names
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=EfSiXFeSa9QWCkmrO5pS1A8YDhVkLhvm&limit=10";
+//var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=EfSiXFeSa9QWCkmrO5pS1A8YDhVkLhvm&limit=10";
 
 //function to create dynamic buttons from topics array
 $(document).ready(function() {
@@ -13,10 +13,12 @@ $(document).ready(function() {
         $("#animal-buttons").append("<button type='button' onclick='searchGif(\"" + topics[i] + "\")' class='btn btn-primary' value=' " + topics[i] + "'> " + topics[i] + " </button>");
     }
 });
-//finction to display gifs to match clicked animal button
+//function to display gifs to match clicked animal button
 function animalButtonClicked() {
     var userInput = $("#animal-buttons").val();
     searchGif(userInput);
+    console.log(userInput);
+    console.log(userInput);
 }
 
 function submitButtonClicked() {
@@ -25,18 +27,19 @@ function submitButtonClicked() {
     if (userInput) {
         $('#animal-buttons').append("<button type='button' onclick='searchGif(\"" + userInput + "\")' class='btn btn-primary' value=' " + userInput + "'> " + userInput + " </button>");
     }
+    console.log(userInput);
 }
 
-function searchGif(topics) {
-
+function searchGif(gifName) {
     $.ajax({
-            url: "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=EfSiXFeSa9QWCkmrO5pS1A8YDhVkLhvm&limit=10",
+            url: "https://api.giphy.com/v1/gifs/search?q=" + gifName + "&api_key=EfSiXFeSa9QWCkmrO5pS1A8YDhVkLhvm&limit=10",
             method: "GET"
         })
         .then(function(response) {
             displayGif(response); //displays the gifs
                 console.log(queryURL);
                 console.log(response);
+                console.log("entered searchGif function");
         })
 }
 
@@ -50,6 +53,7 @@ function displayGif(response) {
 
         image = "<div class='col-md-12'>" + image + "</div>";
         $("#animals").append(image);
+        console.log("entered displayGif function");
     }
 
     $("#animals").on('click', function() {
